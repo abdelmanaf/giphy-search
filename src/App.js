911404e226e0db92db4dc2648cb5e47a486cb4ee
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-04 20:16:41 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+      super(props)
+
+      this.state = {
+        baseURL: 'http://www.omdbapi.com/?',
+        apikey: 'apikey=769a6138',
+        query: '&t=',
+        movieTitle: '',
+        searchURL: ''
+      }
+  }
+
+  handleChange = (event)=> {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  handleSubmit = (event)=> {
+    event.preventDefault()
+    this.setState({
+      searchURL: this.state.baseURL + this.state.apikey + this.state.query +  this.state.movieTitle
+    }, () => {
+      fetch(this.state.searchURL)
+        .then(res => res.json())
+        .then(json => this.setState({
+          movie: json,
+          movieTitle: ''
+        }))
+        .catch(err => console.error(err))
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='movieTitle'>Title</label>
+          <input
+            id='movieTitle'
+            type='text'
+            value={this.state.movieTitle}
+            onChange={this.handleChange}
+          />
+          <input
+            type='submit'
+            value='Find Movie Info'
+          />
+        </form>
+
+        {
+          (this.state.movie) ? <MovieInfo movie={this.state.movie} />  : ''
+        }
+        
+      </>
+    );
+  } 
+}
+
+class MovieInfo extends Component {
+  render () {
+    return  (
+      <div>
+        <h1>Title: {this.props.movie.Title}</h1>
+        <h2>Year: {this.props.movie.Year}</h2>
+        <img src={this.props.movie.Poster} alt={this.props.movie.Title}/>
+        <h3>Genre: {this.props.movie.Genre}</h3>
+        <h4>Plot: {this.props.movie.Plot}</h4>
+      </div>
+    )
+  }
 }
 
 export default App;
